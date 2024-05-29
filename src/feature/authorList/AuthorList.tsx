2bb4423a6b26df927358dd3c945814fa30cde08c
Arch Man <archmanayon@gmail.com>
2024-05-29 22:25:09 +0800

    Setting up for Author list filtration

--- src/feature/authorList/AuthorList.tsx
+++ src/feature/authorList/AuthorList.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import LayoutContainer from '@/components/LayoutContainer'
+import { Row } from '@/hooks/useImport'
+import usePageTitle from '@/hooks/usePageTitle'
+import useRoyalties from '@/hooks/useRoyalties'
+import { ColDef } from 'ag-grid-community'
+import { SetStateAction, useCallback, useEffect, useRef, useState } from 'react'
+import { AgGridReact } from 'ag-grid-react'
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-quartz.css'
+import LoopIcon from '@mui/icons-material/Loop'
 
 const AuthorList = () => {
-  return <div>AuthorList</div>
+  return (
+    <LayoutContainer title="Author List">
+      <div className="h-[80vh]">
+        <div className="flex h-full w-full items-center justify-center">
+          Whoops! We did not find anything to show here.
+        </div>
+      </div>
+    </LayoutContainer>
+  )
 }
 
 export default AuthorList
